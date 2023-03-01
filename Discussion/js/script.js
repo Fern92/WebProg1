abb6f4f38871144d76a1f7021f6c4309e48ff791
Fernando Dela Riva <delariva.fernando@cec.edu.ph>
2023-03-01 23:19:38 +0800

    Discussio

---
+++ Discussion/js/script.js
@@ -0,0 +1,11 @@
+// function myfunction(){
+//     var x, y;
+//     x = 5, y = 7;
+//     let sum = x + y;
+    
+//     document.getElementById("demo").innerHTML = sum
+//     document.getElementById("demo").style.color = "blue";
+//     document.getElementById("demo").style.fontSize = "80px";
+//     document.getElementById("demo").innerHTML = new Date;
+// }
+
